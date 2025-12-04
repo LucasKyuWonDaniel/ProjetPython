76fee57801257b44790e2847afa15da44456a04f
LucasKyuWonDaniel <kyu.shim@efrei.net>
2025-12-04 15:41:48 +0100

    Update Maisons.py

--- Poudlard/univers/Maisons.py
+++ Poudlard/univers/Maisons.py
@@ -1,9 +1,9 @@
 def actualiser_points_maison(maisons, nom_maison, points) :
-  if nom_maison in maisons :
-    maisons[nom_maison] += points
-    print (nom_maison, ":", maisons[nom_maison])
-  else :
-    return " la maison est introuvable"
+    if nom_maison in maisons :
+        maisons[nom_maison] += points
+        print (nom_maison, ":", maisons[nom_maison])
+    else :
+        return " la maison est introuvable"
 
 def afficher_maison_gagnante(maisons) :
     L_maison_gagnante = []
@@ -73,4 +73,5 @@ questions = [
     )
 
 ]
+
 
