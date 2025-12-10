8b426e57c24849a6fc3d91e1061243bed5307bc8
LucasKyuWonDaniel <kyu.shim@efrei.net>
2025-12-10 19:08:58 +0100

    Update chapitre_2.py

--- Poudlard/chapitres/chapitre_2.py
+++ Poudlard/chapitres/chapitre_2.py
@@ -8,7 +8,7 @@ def rencontrer_amis(joueur) :
     print("— Salut ! Moi c’est Ron Weasley. Tu veux bien qu’on s’assoie ensemble ? ")
     print("Que répondez-vous ?")
     L_réponse1 = ["Bien sûr, assieds-toi !", "Désolé, je préfère voyager seul."]
-    réponse1 = demander_choix("Votre choix :", L_réponse1)
+    réponse1 = demander_choix("", L_réponse1)
     if réponse1 == 1 :
         joueur["Attributs"]["loyauté"] += 1
         print("Ron sourit : — Génial ! Tu verras, Poudlard, c’est incroyable ! ")
@@ -20,7 +20,7 @@ def rencontrer_amis(joueur) :
     print("— Bonjour, je m’appelle Hermione Granger. Vous avez déjà lu ‘Histoire de la Magie’ ?")
     print("Que répondez-vous ?")
     L_réponse2 = ["Oui, j’adore apprendre de nouvelles choses !", "Euh… non, je préfère les aventures aux bouquins."]
-    réponse2 = demander_choix("Votre choix :", L_réponse2)
+    réponse2 = demander_choix("", L_réponse2)
     if réponse2 == 1 :
         joueur["Attributs"]["intelligence"] += 1
         print("Hermione : — Super! Dans Histoire de la Magie, quel événement majeur Bathilda Tourdesac identifie-t-elle comme la cause première de l’adoption du Code International du Secret Magique ?")
@@ -33,7 +33,7 @@ def rencontrer_amis(joueur) :
     print("— Je suis Drago Malefoy. Mieux vaut bien choisir ses amis dès le départ, tu ne crois pas ?")
     print("Comment réagissez-vous ? ")
     L_réponse3 = ["Je lui serre la main poliment.", "Je l’ignore complètement.", "Je lui réponds avec arrogance."]
-    réponse3 = demander_choix("Votre choix :", L_réponse3)
+    réponse3 = demander_choix("", L_réponse3)
     if réponse3 == 1 :
         joueur["Attributs"]["ambition"] += 1
         print("Drago : — HaHaHa! ")
@@ -99,6 +99,7 @@ j1 = {
 
 
 lancer_chapitre_2(j1)
+
 
 
 
